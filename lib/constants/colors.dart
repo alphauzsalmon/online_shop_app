750d38627310e037a30cf7eac874c81ef194c163
alphauzsalmon <alphauzsalmon@gmail.com>
2022-08-31 19:19:22 +0500

    Began this app

---
+++ lib/constants/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/cupertino.dart';
+
+const Color lightColor = Color(0xffFF6E4E);
+const Color boldColor = Color(0xff010035);
---
+++ lib/constants/typography.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+TextStyle markPro800(Color color) => TextStyle(
+      fontFamily: "Mark Pro",
+      fontSize: 60.0.sp,
+      fontWeight: FontWeight.w800,
+      height: 66.06 / 60.0,
+      letterSpacing: -0.33.sp,
+      color: color,
+    );
+
+TextStyle markPro700(Color color) => TextStyle(
+      fontFamily: "Mark Pro",
+      fontSize: 60.0.sp,
+      fontWeight: FontWeight.w700,
+      height: 66.06 / 60.0,
+      letterSpacing: -0.33.sp,
+      color: color,
+    );
+
+TextStyle markPro500(Color color) => TextStyle(
+      fontFamily: "Mark Pro",
+      fontSize: 60.0.sp,
+      fontWeight: FontWeight.w500,
+      height: 66.06 / 60.0,
+      letterSpacing: -0.33.sp,
+      color: color,
+    );
+
+TextStyle markPro400(Color color) => TextStyle(
+      fontFamily: "Mark Pro",
+      fontSize: 60.0.sp,
+      fontWeight: FontWeight.w400,
+      height: 66.06 / 60.0,
+      letterSpacing: -0.33.sp,
+      color: color,
+    );
